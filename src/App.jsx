d36d059fe55fc90a8248274e71ed0f633435c37b
Kheuzayni <bayecheikhfall249@gmail.com>
2024-02-05 20:37:35 +0000

    Use api

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,19 @@ function App() {
   useEffect(() => {
     fetch(`http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`)
     .then(response => {
-    console.log(response);
-    return response.json()
-  })
+      console.log(response);
+      return response.json()
+    })
+    .then(responseData => {
+      console.log(responseData);
+      setWeatherData({
+        city: responseData.data.city,
+        country: responseData.data.country,
+        iconId: responseData.data.ic,
+        temperature: responseData.data.curent.weather.tp,
+      })
+    })
+
   }, [])
 
 
@@ -20,15 +30,28 @@ function App() {
   return (
 
       <main>
-        <div className="loader-container">
+        <div className={`loader-container ${!weatherData && "active"}`}>
           <img src = {loader} alt="loading icon" />
         </div> 
+        ////////////////////////////////////////////////////////////////
+        <p>Test données api</p>
         <p className="city-name">Dakar</p>
         <p className="country-name">Sénégal</p>
         <p className="temperature-name">30 °</p>
         <div className="info-icon-container">
           <img src = "/icons/01d.svg" className="" alt="weather icon" />
         </div>
+        
+        <br/><br/><br/>
+        ////////////////////////////////////////////////////////////////
+        <p>Test données statique</p>
+        <p className="city-name">Dakar</p>
+        <p className="country-name">Sénégal</p>
+        <p className="temperature-name">30 °</p>
+        <div className="info-icon-container">
+          <img src = "/icons/01d.svg" className="" alt="weather icon" />
+        </div>
+        ////////////////////////////////////////////////////////////////
       </main>
   
   );
